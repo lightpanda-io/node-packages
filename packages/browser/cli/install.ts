f0004ee2bd0a48502306d5546449082608cc28aa
Nicolas Rigaudiere <i.am.a@curious.engineer>
2025-08-14 18:36:30 +0200

    fix: cli install path

--- packages/browser/cli/install.ts
+++ packages/browser/cli/install.ts
@@ -3,4 +3,11 @@
 import packageJson from './../package.json' with { type: 'json' }
 import { download } from './../src/download'
 
-download(`${packageJson.name}/dist/lightpanda`)
+const install = async () => {
+  console.info('⏳ Downloading latest version of Lightpanda browser…')
+
+  await download(`node_modules/${packageJson.name}/dist/lightpanda`)
+  console.info('✅ Download finished!')
+}
+
+install()
